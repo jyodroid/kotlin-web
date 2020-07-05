2d7347a459a1c717a3ab57262174658f848377aa
John Tangarife <jyodroid@gmail.com>
2020-07-05 14:08:34 -0500

    Api Module

---
+++ paganinisoundsApi/src/main/kotlin/com/jyodroid/paganinisounds/controllers/SongsController.kt
@@ -0,0 +1,27 @@
+package com.jyodroid.paganinisounds.controllers
+
+import com.jyodroid.paganinisounds.model.Song
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SongsController {
+    val unwatchedVideos = listOf(
+        Song(1, "Caprice 24", "Alexandrox Xandrox", "https://www.youtube.com/watch?v=CjDz-r65xUU"),
+        Song(2, "Caprice 5", "Sumina Studer", "https://www.youtube.com/watch?v=0jXXWBt5URw"),
+        Song(3, "La Campanella", "Stefan Milenkovich", "https://www.youtube.com/watch?v=230RgLax-_o")
+    )
+
+    val watchedVideos = listOf(
+        Song(4, "Allegro maestoso", "Stepan Grytsay", "https://www.youtube.com/watch?v=L1JeOD0xvuc")
+    )
+
+    @GetMapping("/songs")
+    fun getSong(@RequestParam(value = "type", defaultValue = "watched")type: String): List<Song> {
+        if (type == "unwatched"){
+            return unwatchedVideos
+        }
+        return watchedVideos
+    }
+}
---
+++ paganinisoundsApi/src/main/kotlin/com/jyodroid/paganinisounds/model/Song.kt
@@ -0,0 +1,3 @@
+package com.jyodroid.paganinisounds.model
+
+data class Song(val id: Int, val title: String, val interpreter: String, val videoUrl: String)
