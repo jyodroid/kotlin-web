0d0bc3d691a832234b5852aa2059599fb1feb5c9
John Tangarife <jyodroid@gmail.com>
2020-07-05 14:09:04 -0500

    Api Module

---
+++ paganinisoundsApi/src/main/kotlin/com/jyodroid/paganinisounds/PaganinisoundsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.jyodroid.paganinisounds
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaganinisoundsApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaganinisoundsApiApplication>(*args)
+}
---
+++ paganinisoundsApi/src/test/kotlin/com/jyodroid/paganinisounds/PaganinisoundsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jyodroid.paganinisounds
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaganinisoundsApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
